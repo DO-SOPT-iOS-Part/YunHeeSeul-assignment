395b3a65b846c91756911286ea3788c520310643
seuriseuljjeok <heeseul09@naver.com>
2023-10-16 17:06:59 +0900

    프로젝트 세팅

---
+++ weather-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather-app
+//
+//  Created by 윤희슬 on 2023/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
